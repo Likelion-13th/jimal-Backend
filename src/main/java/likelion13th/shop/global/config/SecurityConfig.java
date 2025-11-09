2fbc6a967ff203617f4ef13606d2cd23fff3a547
qkwltkwkd1 <wldndi2@kau.kr>
2025-11-09 17:01:05 +0900

    hhh

--- src/main/java/likelion13th/shop/global/config/SecurityConfig.java
+++ src/main/java/likelion13th/shop/global/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:3000",
-                "http://sajang-dev.ap-northeast-2.elasticbeanstalk.com",
+                "http://Sajang-dev-env.eba-4qx9gwxi.ap-northeast-2.elasticbeanstalk.com",
                 "https://jimalshop.netlify.app"
 
         ));
